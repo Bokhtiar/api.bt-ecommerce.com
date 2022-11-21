366b1bb240e38557337600c313682a4b493eec7e
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-11-22 01:56:17 +0600

    updated

--- src/controllers/user/cart.controller.ts
+++ src/controllers/user/cart.controller.ts
@@ -1,20 +1,18 @@
 import { Request, Response, NextFunction } from 'express'
 import { Types } from 'mongoose'
-import { IUserCreateUpdate } from 'src/types/user/user.types'
 import { cartService } from '../../services/user/cart.service'
-import { ICartCreate } from '../../types/user/cart.types'
+
 
 /* specidic user find all cart */
-export const index = async(req: Request, res: Response, next: NextFunction) => {
+export const index = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {id} = req.user
-        const results = await cartService.findAll({_id: new Types.ObjectId(id)})
+        const { id } = req.user
+        const results = await cartService.findAll({ _id: new Types.ObjectId(id) })
         res.status(200).json({
-            status:true,
+            status: true,
             data: results
         })
-
-    } catch (error:any) {
+    } catch (error: any) {
         console.log(error);
         next(error)
     }
